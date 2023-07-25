06bd055c1a6776579b419f50c07c119cfa98b57d
Nazrul Islam Nayan <nazrulislamnayan7@gmail.com>
2023-07-25 16:34:22 +0600

    converted from plugin to module

--- optemiz-dashboard.php
+++ optemiz-dashboard.php
@@ -1,26 +1,4 @@
 <?php
-/**
- * Plugin Name: Optemiz Dashboard
- * Plugin URI: https://optemiz.com
- * Description: This plugin shows faq question and answers for per product with comment and FAQ schema support.
- * Version: 1.0.0
- * Author: Optemiz
- * Author URI: https://optemiz.com/
- * Text Domain: optemiz-dashboard
- * Domain Path: /i18n/languages/
- *
- * WP Requirement & Test
- * Requires at least: 4.4
- * Tested up to: 6.0
- * Requires PHP: 5.6
- *
- * WC Requirement & Test
- * WC requires at least: 3.2
- * WC tested up to: 7.0
- *
- * @package Optemiz
- */
-
 use OptDashboard\OptemizDashboard;
 use OptDashboard\OptemizSettings;
 use OptDashboard\OptemizTab;
