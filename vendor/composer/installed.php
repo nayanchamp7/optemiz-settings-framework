4727584fb1e13f099501481546321c70353e8632
Nazrul Islam Nayan <nazrulislamnayan7@gmail.com>
2024-09-29 15:25:04 +0600

    added vendor files

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'optemiz/dashboard',
+        'pretty_version' => '1.0.1',
+        'version' => '1.0.1.0',
+        'reference' => NULL,
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'optemiz/dashboard' => array(
+            'pretty_version' => '1.0.1',
+            'version' => '1.0.1.0',
+            'reference' => NULL,
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
